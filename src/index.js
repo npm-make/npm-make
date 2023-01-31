ad3ad785eaf96a7bb2ef36e895396b38f633f62b
gaomingyu <874774259@qq.com>
2023-02-01 00:05:35 +0800

    search

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import Search from './core/search.js'
+import child_process from 'node:child_process'
+import string_decoder from 'node:string_decoder'
+import iconv from 'iconv-lite'
 
-let t1 = Date.now()
+iconv.skipDecodeWarning = true
 
-let a = new Search()
-await a.search('C:\\Project', '**/zlib/**/*.c')
+// let a = new Search()
+// await a.search('C:\\Project', '**/zlib/**/*.c')
+//
+// for (let file of a.result) {
+//     console.log(file.path)
+//     child_process.exec(
+//         `"C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Tools\\MSVC\\14.34.31933\\bin\\Hostx64\x64\\cl.exe" "${file.path}"`,
+//         (error, stdout, stderr) => {
+//             console.log(error)
+//         }
+//     )
+// }
 
-let t2 = Date.now()
-
-console.log(a)
-console.log(t2 - t1)
+child_process.exec('"C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Tools\\MSVC\\14.34.31933\\bin\\Hostx64\x64\\cl.exe" -utf-8 "C:\\Project\\npm-make\\zlib\\adler32.c"',
+    (error, stdout, stderr) => {
+        console.log(iconv.decode(stdout, 'gb2312'))
+        console.log(iconv.decode(stderr, 'gb2312'))
+    }
+)
