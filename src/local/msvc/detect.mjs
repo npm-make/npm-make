bf7c84c48032c44c3bd12a4bb6efd1084b20d611
gaomingyu <874774259@qq.com>
2023-02-02 23:56:47 +0800

    msvc自动检测

--- src/local/msvc/detect.mjs
+++ src/local/msvc/detect.mjs
@@ -1,3 +1,6 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+
 export default class {
     static async detect(env) {
         env.MSVC_CL = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Tools\\MSVC\\14.34.31933\\bin\\Hostx64\\x64\\cl.exe'
@@ -15,5 +18,12 @@ export default class {
             'C:\\Program Files (x86)\\Windows Kits\\10\\lib\\10.0.22000.0\\ucrt\\x64',
             'C:\\Program Files (x86)\\Windows Kits\\10\\lib\\10.0.22000.0\\um\\x64'
         ]
+
+        // let instances = await fs.opendir('C:\\ProgramData\\Microsoft\\VisualStudio\\Packages\\_Instances')
+        // for await (let instance of instances) {
+        //     let statePath = path.resolve(instances.path, instance.name, 'state.json')
+        //     let stateText = await fs.readFile(statePath, 'utf-8')
+        //     let stateJson = JSON.parse(stateText)
+        // }
     }
 }
