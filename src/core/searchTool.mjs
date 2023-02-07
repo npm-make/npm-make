b17961519d0918ee14fc97a9e70a28fefdff75b8
gaomingyu <874774259@qq.com>
2023-02-07 11:06:44 +0800

    1

--- src/core/searchTool.mjs
+++ src/core/searchTool.mjs
@@ -1,41 +1,17 @@
 import fs from 'node:fs/promises'
-import Project from './project.mjs'
-import Target from './target.mjs'
 
 export default class {
     static #ignoreDir = /^node_modules|^npm_make|^\./
 
-    static async searchProject(project) {
-        if (project instanceof Project) {
-            await this.#searchPath(project.projectFileList, project.projectPath, '')
-            for (let target of project.targetList) {
-                this.#searchTarget(project.projectFileList, target)
-            }
-        }
-    }
-
-    static async #searchPath(projectFileList, projectPath, thisPath) {
-        let dir = await fs.opendir(projectPath + thisPath)
+    static async searchPath(output, base, last) {
+        let dir = await fs.opendir(base + last)
         for await (let item of dir) {
             if (item.isDirectory()) {
                 if (!this.#ignoreDir.test(item.name)) {
-                    await this.#searchPath(projectFileList, projectPath, thisPath + '/' + item.name)
+                    await this.searchPath(output, base, last + '/' + item.name)
                 }
             } else {
-                projectFileList.push(thisPath + '/' + item.name)
-            }
-        }
-    }
-
-    static #searchTarget(projectFileList, target) {
-        if (target instanceof Target) {
-            if (target.sourcePatternList.length > 0) {
-                let regex = new RegExp(target.sourcePatternList.join('|'))
-                for (let projectFile of projectFileList) {
-                    if (regex.test(projectFile)) {
-                        target.sourceList.push(projectFile)
-                    }
-                }
+                output.push(last + '/' + item.name)
             }
         }
     }
