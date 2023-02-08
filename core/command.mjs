67ab9054ceabcb692577af4ac762bdc486d7a474
gaomingyu <874774259@qq.com>
2023-02-08 22:22:02 +0800

    0.0.5

--- core/command.mjs
+++ core/command.mjs
@@ -2,10 +2,14 @@ import child_process from 'node:child_process'
 import process from 'node:process'
 
 export default class Self {
-    static changeEncoding() {
+    static {
         if (process.platform === 'win32') {
             //noinspection SpellCheckingInspection
             child_process.execSync('chcp 65001')
         }
+    }
+
+    static build() {
+
     }
 }
