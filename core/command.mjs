c13bea4dffc4173b4e6d8d6406afd37ddd0f32ca
gaomingyu <874774259@qq.com>
2023-02-09 00:18:05 +0800

    command

--- core/command.mjs
+++ core/command.mjs
@@ -1,15 +1,17 @@
 import child_process from 'node:child_process'
 import process from 'node:process'
 
+//noinspection all
 export default class Self {
     static {
         if (process.platform === 'win32') {
-            //noinspection SpellCheckingInspection
             child_process.execSync('chcp 65001')
         }
     }
 
-    static build() {
+    static async build() {
+    }
 
+    static async help() {
     }
 }
--- index.mjs
+++ index.mjs
@@ -1,3 +1,21 @@
 import process from 'node:process'
+import command from './core/command.mjs'
 
-console.log(process.argv)
+let inputList = process.argv.slice(2)
+let beforeMethod = []
+let method = null
+let afterMethod = []
+for (let input of inputList) {
+    if (method) {
+        afterMethod.push(input)
+    } else if (input in command) {
+        method = input
+    } else {
+        beforeMethod.push(input)
+    }
+}
+if (method) {
+    await command[method](beforeMethod, afterMethod)
+} else {
+    await command.help()
+}
