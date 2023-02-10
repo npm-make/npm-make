c263e0471dfbc01ceadcd2d13390aa0a366d39d5
gaomingyu <874774259@qq.com>
2023-02-11 01:05:04 +0800

    1

--- core/executeTool.mjs
+++ core/executeTool.mjs
@@ -1,10 +1,10 @@
 import child_process from 'node:child_process'
 import process from 'node:process'
 
+//noinspection SpellCheckingInspection
 export default class Self {
     static {
         if (process.platform === 'win32') {
-            //noinspection SpellCheckingInspection
             child_process.execSync('chcp 65001')
         }
     }
