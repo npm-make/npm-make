9071b263cc45b50513a8cc80262eff88ebaae695
gaomingyu <874774259@qq.com>
2023-02-17 23:38:34 +0800

    searchTool

--- core/searchTool.mjs
+++ core/searchTool.mjs
@@ -3,6 +3,23 @@ import fs from 'node:fs/promises'
 export default class {
     static #ignoreDir = /^node_modules|^npm_make|^\./
 
+    /**
+     * @param {InterfaceProject} project
+     */
+    static async searchProject(project) {
+        await this.searchPath(project.projectFileList, project.projectPath, '')
+        for (let target of project.targetList) {
+            for (let sourcePattern of target.sourcePatternList) {
+                let regex = this.searchRegex(sourcePattern)
+                for (let projectFile of project.projectFileList) {
+                    if (regex.test(projectFile)) {
+                        target.sourceList.push(projectFile)
+                    }
+                }
+            }
+        }
+    }
+
     static async searchPath(output, base, last) {
         let dir = await fs.opendir(base + last)
         for await (let item of dir) {
@@ -16,27 +33,9 @@ export default class {
         }
     }
 
-    /**
-     * @param {core.Project} project
-     */
-    static async searchProject(project) {
-        await this.searchPath(project.projectFileList, project.projectPath, '')
-        for (let target of project.targetList) {
-            if (target.sourcePatternList.length > 0) {
-                let patternList = []
-                for (let sourcePattern of target.sourcePatternList) {
-                    let temp1 = sourcePattern.replaceAll(/[.?+(){}\[\]\\^$|]/g, '\\$&')
-                    let temp2 = temp1.replaceAll('*', '[^/]*')
-                    let temp3 = temp2.replaceAll('[^/]*[^/]*', '.*')
-                    patternList.push('^/(' + temp3 + ')$')
-                }
-                let regex = new RegExp(patternList.join('|'))
-                for (let projectFile of project.projectFileList) {
-                    if (regex.test(projectFile)) {
-                        target.sourceList.push(projectFile)
-                    }
-                }
-            }
-        }
+    static searchRegex(pattern) {
+        let temp1 = pattern.replaceAll(/[.?+^$|(){}\[\]\\]/g, '\\$&')
+        let temp2 = temp1.replaceAll(/\*+/g, ret => ret === '*' ? '[^/]*' : '.*')
+        return new RegExp('^/(' + temp2 + ')$')
     }
 }
