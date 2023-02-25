d847e652247a1ac09440094b85ebf3a473fb0023
gaomingyu <874774259@qq.com>
2023-02-25 13:47:33 +0800

    msvc rc

--- source/toolchain/msvc/rc.ts
+++ source/toolchain/msvc/rc.ts
@@ -1,16 +1,24 @@
 import msvc from './msvc'
+import BuildFeature from '../../project/buildFeature'
 import Source from '../../project/source'
+import Target from '../../project/target'
 
 export default class {
-    static async build(source: Source) {
+    static async build(buildFeature: BuildFeature, target: Target, source: Source) {
         let flagList = Array.from(source.optionList)
         for (let definition of source.definitionList) {
-            flagList.push('/D' + definition)
+            flagList.push('/d' + definition)
+        }
+        for (let definition of target.definitionList) {
+            flagList.push('/d' + definition)
         }
         for (let includePath of source.includePathList) {
-            flagList.push('/I' + includePath)
+            flagList.push('/i' + includePath)
         }
-        flagList.push('/Fo' + source.objectPrefix + '.res')
+        for (let includePath of target.includePathList) {
+            flagList.push('/i' + includePath)
+        }
+        flagList.push('/fo' + source.objectPrefix + '.res')
         flagList.push('/nologo')
         //这些命令在末尾
         flagList.push(source.sourcePath)
