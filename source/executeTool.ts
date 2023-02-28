5b0021cc023518eec8c4fb86b668d643e5686170
gaomingyu <874774259@qq.com>
2023-02-28 14:25:40 +0800

    executeTool

--- source/executeTool.ts
+++ source/executeTool.ts
@@ -2,9 +2,9 @@ import child_process from 'node:child_process'
 import process from 'node:process'
 
 export default class Self {
-    private static limit = 10
-    private static running = 0
-    private static queue = []
+    static #limit = 10
+    static #running = 0
+    static #queue = []
     static {
         if (process.platform === 'win32') {
             child_process.execSync('chcp 65001')
@@ -16,22 +16,22 @@ export default class Self {
             function thisTask() {
                 function taskDone(error, stdout, stderr) {
                     resolve({error, stdout, stderr})
-                    let nextTask = Self.queue.shift()
+                    let nextTask = Self.#queue.shift()
                     if (nextTask) {
                         nextTask()
                     } else {
-                        Self.running--
+                        Self.#running--
                     }
                 }
 
                 child_process.execFile(file, args, options, taskDone)
             }
 
-            if (Self.running < Self.limit) {
-                Self.running++
+            if (Self.#running < Self.#limit) {
+                Self.#running++
                 thisTask()
             } else {
-                Self.queue.push(thisTask)
+                Self.#queue.push(thisTask)
             }
         }
 
