f87b3a2b207f0775ffe7233c0fee29945c317553
gaomingyu <874774259@qq.com>
2023-03-01 11:03:02 +0800

    正则表达式

--- source/regexTool.ts
+++ source/regexTool.ts
@@ -1,5 +1,8 @@
 export default class {
     static path(pattern: string): RegExp {
-        return new RegExp(pattern)
+        const pattern1 = pattern.replace(/[.+^$|(){}\[\]\\]/g, '\\$&')
+        const pattern2 = pattern1.replace(/\*/g, '.*')
+        const pattern3 = pattern2.replace(/\?/g, '[^/]*')
+        return new RegExp('^/' + pattern3 + '$')
     }
 }
