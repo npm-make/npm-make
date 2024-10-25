43098a160b661d7d0a4c2ed7b76ac04cdf4d04dd
gaomingyu <874774259@qq.com>
2024-10-25 14:25:40 +0800

    Executor

---
+++ core/Executor.mjs
@@ -0,0 +1,21 @@
+import { execFile } from 'node:child_process'
+
+export class Executor {
+    cwd
+    env
+    encoding = 'utf-8'
+    shell = false
+    windowsHide = true
+
+    async execute(file, ...args) {
+        return new Promise((resolve, reject) => {
+            execFile(file, args, this, (error, stdout, stderr) => {
+                if (error) {
+                    reject({ error, stdout, stderr })
+                } else {
+                    resolve({ error, stdout, stderr })
+                }
+            })
+        })
+    }
+}
